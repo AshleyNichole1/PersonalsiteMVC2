06a358d1090c26b3ff6d50592b7a660c807f15e8
AshleyNichole1 <ashleynicholelea@outlook.com>
2021-11-05 10:08:28 -0500

    Template conversion

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -26,5 +30,57 @@ namespace WebApplication1.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ContactAction(ContactAction cam)
+        {
+            //If NOT valid
+            if (!ModelState.IsValid)
+            {
+                return View(cam);
+            }
+
+            string message = $"You have received an email from {cam.Name} with a subject:" +
+                $"{cam.Subject}. Please respond to {cam.Email} with your response to the " +
+                $"following message: <br/>{cam.Message}";
+
+            MailMessage mm = new MailMessage(
+
+                ConfigurationManager.AppSettings["EmailUser"].ToString(),
+                ConfigurationManager.AppSettings["EmailTo"].ToString(),
+                cam.Subject,
+                message);
+
+            mm.IsBodyHtml = true;
+
+            mm.Priority = MailPriority.High; //Not sure if I want to keep this
+
+            mm.ReplyToList.Add(cam.Email);
+
+            SmtpClient client = new SmtpClient(ConfigurationManager.AppSettings["EmailClient"].ToString());
+
+            client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["EmailUser"].ToString(),
+            ConfigurationManager.AppSettings["EmailPass"].ToString());
+            client.Port = 8889;
+
+            try
+            {
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.CustomerMessage = $"Sorry, We are working on Email Functionality. Please try again later.<br/>Error Message:" +
+                    $"<br/>{ex.StackTrace}";
+
+                return View(cam);
+            }
+
+
+
+            return View("EmailConfirmation", cam);
+        }
+
+
     }
 }
---
+++ WebApplication1/Models/ContactAction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class ContactAction
+    {
+
+        [Required(ErrorMessage = "*")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [UIHint("MultilineText")] //Larger Text Box
+        public string Message { get; set; }
+
+    }
+
+}
